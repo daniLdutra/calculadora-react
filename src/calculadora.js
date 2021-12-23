d6113478e457986a32b0b4c9d1ef3575496ea4cc
Daniella Dutra <danivetdutra@gmail.com>
2021-12-23 15:07:27 -0300

    Interface e design concluidos

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,11 +1,89 @@
 import './calculadora.css';
+import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 
 function Calculadora() {
   return (
-    <>
-      {' '}
-      <h1>Calculadora</h1>
-    </>
+    <header
+      style={{
+        background: 'transparent !import',
+        backgroundColor: '#007bff',
+        padding: '5px',
+        margin: '5px',
+        width: '400px',
+      }}
+    >
+      <Container>
+        <Row>
+          <Col xs="3">
+            <Button variant="danger">C</Button>
+          </Col>
+          <Col xs="9">
+            <Form.Control
+              type="text"
+              name="txtNumeros"
+              class="text-right"
+              readOnly="readonly"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="light">7</Button>
+          </Col>
+          <Col>
+            <Button variant="light">8</Button>
+          </Col>
+          <Col>
+            <Button variant="light">9</Button>
+          </Col>
+          <Col>
+            <Button variant="warning">/</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="light">4</Button>
+          </Col>
+          <Col>
+            <Button variant="light">5</Button>
+          </Col>
+          <Col>
+            <Button variant="light">6</Button>
+          </Col>
+          <Col>
+            <Button variant="warning">*</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="light">1</Button>
+          </Col>
+          <Col>
+            <Button variant="light">2</Button>
+          </Col>
+          <Col>
+            <Button variant="light">3</Button>
+          </Col>
+          <Col>
+            <Button variant="warning">-</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="light">0</Button>
+          </Col>
+          <Col>
+            <Button variant="light">.</Button>
+          </Col>
+          <Col>
+            <Button variant="success">=</Button>
+          </Col>
+          <Col>
+            <Button variant="warning">+</Button>
+          </Col>
+        </Row>
+      </Container>
+    </header>
   );
 }
 
