edfd2875ce4331cab9a22d295982369f17faeca7
Daniella Dutra <danivetdutra@gmail.com>
2021-12-27 13:24:44 -0300

    Adicionado testes para operacoes aritmeticas

---
+++ src/calculadora.service.js
@@ -0,0 +1,30 @@
+function CalculadoraService() {
+  const SOMA = '+';
+  const SUBTRACAO = '-';
+  const DIVISAO = '/';
+  const MULTIPLICACAO = '*';
+
+  function calcular(numero1, numero2, operacao) {
+    let resultado;
+    switch (operacao) {
+      case SOMA:
+        resultado = numero1 + numero2;
+        break;
+      case SUBTRACAO:
+        resultado = numero1 - numero2;
+        break;
+      case DIVISAO:
+        resultado = numero1 / numero2;
+        break;
+      case MULTIPLICACAO:
+        resultado = numero1 * numero2;
+        break;
+    }
+
+    return resultado;
+  }
+
+  return [calcular, SOMA, SUBTRACAO, DIVISAO, MULTIPLICACAO];
+}
+
+export default CalculadoraService;
--- src/calculadora.service.test.js
+++ src/calculadora.service.test.js
@@ -1,11 +1,24 @@
 import React from 'react-dom';
-import CalculadoraService from '.calculadora.service';
+import CalculadoraService from './calculadora.service';
 
 describe('Teste do CalculadoraService', () => {
-  const [calcular] = CalculadoraService();
+  const [calcular, SOMA, SUBTRACAO, DIVISAO, MULTIPLICACAO] =
+    CalculadoraService();
 
   it('deve garantir que 1 + 4 = 5.', () => {
-    let soma = calcular(1, 4, '+');
+    let soma = calcular(1, 4, SOMA);
     expect(soma).toEqual(5);
+  });
+  it('deve garantir que 4 - 1 = 3.', () => {
+    let subtracao = calcular(4, 1, SUBTRACAO);
+    expect(subtracao).toEqual(3);
+  });
+  it('deve garantir que 4 / 1 = 4.', () => {
+    let divisao = calcular(4, 1, DIVISAO);
+    expect(divisao).toEqual(4);
+  });
+  it('deve garantir que 4 * 1 = 4.', () => {
+    let multiplicacao = calcular(4, 1, MULTIPLICACAO);
+    expect(multiplicacao).toEqual(4);
   });
 });
